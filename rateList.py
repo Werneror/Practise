2b6263b195eea9b9cd0b162b5b1bd2fbf6f6fa32
wangning <2073211851@qq.com>
2016-03-10 11:08:00 +0800

    修正了日志输出错误

--- rateList.py
+++ rateList.py
@@ -1,5 +1,6 @@
 # -*- coding: utf-8 -*-
 import re
+import os
 import sys
 import time
 import MySQLdb
@@ -29,17 +30,18 @@ headersParameters = {
     'User-Agent': 'Mozilla/6.1 (Windows NT 6.3; WOW64; Trident/7.0; rv:11.0) like Gecko'
     }
 
-def printlog(message, data, number, i):
+def printlog(message, data, number, i, logfile):
     '''该函数向logfile输出日志信息，虽然可以输出中文信息，
 	但有些平台不支持中文显示，建议使用英文'''
     nowtime = strftime("%Y-%m-%d,%H:%M:%S", localtime())
-    log = u"[{0},number={1},page={2}]{3}\n{4}\n\n".format(nowtime, number, i, message, data)
+    log = u"[{0},number={1},page={2}]{3}\t{4}\n".format(nowtime, number, i, message, data)
     logfile.write(log)
 #END OF printlog
 
-def rateList2mysql(itemId,sellerId):
+def rateList2mysql(itemId, sellerId, logfile):
     '''该函数将指定商品的评论信息保存到mysql数据库中，
-    itemId是商品id，sellerid是卖家id'''
+    itemId是商品id，sellerid是卖家id
+    同时日志信息会保存在logfile中'''
     oldjson = ''
     i = 0   #i表示页码
     while True:
@@ -48,44 +50,43 @@ def rateList2mysql(itemId,sellerId):
         urlparams = {'itemId':str(itemId),'sellerId':str(sellerId),'currentPage':str(i)}
         try:
             r = requests.get(url,params=urlparams,headers=headersParameters)
-            printlog(u'Try to open the url:', r.url, number, i)
+            printlog(u'Try to open the url:', r.url, number, i, logfile)
         except requests.exceptions.ConnectionError:
-            print u"网络连接超时，十秒后重试。"
             time.sleep(10)
             try:
                 r = requests.get(url,params=urlparams,headers=headersParameters)
-                printlog(u'Try to open the url again:', r.url, number, i)
+                printlog(u'Try to open the url again:', r.url, number, i, logfile)
             except requests.exceptions.ConnectionError:
-                printlog(u'Failed to open the url.', u'ERROR', number, i)
+                printlog(u'Failed to open the url.', u'ERROR', number, i, logfile)
                 continue
         
-        printlog(u'Succeed in opening the url.', u'OK', number, i)
+        printlog(u'Succeed in opening the url.', u'OK', number, i, logfile)
         time.sleep(0.1)
         try:
             response = unicode(r.content, r.encoding).encode('UTF-8')
-            printlog(u'Try to unicode the content.', u'waiting...', number, i)
+            printlog(u'Try to unicode the content.', u'waiting...', number, i, logfile)
         except UnicodeDecodeError:
-            printlog(u'Failed to unicode the content.The content is:', r.content, number, i)
+            printlog(u'Failed to unicode the content.', u'ERROR', number, i, logfile)
             time.sleep(1)
             if i > 99:
                 return
         else:
-            printlog(u'Succeed in unicoding the content.', u'OK', number, i)
+            printlog(u'Succeed in unicoding the content.', u'OK', number, i, logfile)
             #去除天猫返回的json中不和json规范的部分，即只保留中括号中的内容
             #用''.join()而不用[0]索引是为了防止匹配不成功出发错误中断程序
-            printlog(u'Try to regular expression matching.', u'waiting', number, i)
+            printlog(u'Try to regular expression matching.', u'waiting', number, i, logfile)
             newjson = ''.join(re.findall(r'\"rateList\":(\[.*?\])\,\"searchinfo\"',response))
             if newjson == '':#如果匹配失败，则尝试老版本的正则
                 newjson = ''.join(re.findall(r'\"rateList\":(\[.*?\])\,\"tags\"',response))
             if newjson =='':
-                printlog(u'Regual matching results are null.', u'WARN', number, i)
-            printlog(u'Succeed in regularing expression matching.', u'OK', number, i)
+                printlog(u'Regual matching results are null.', u'WARN', number, i, logfile)
+            printlog(u'Succeed in regularing expression matching.', u'OK', number, i, logfile)
             if newjson == oldjson:  #当请求页码超过最大页码后，返回页面为最大页码页面，以此作为结束条件
-                printlog(u'Finished collceting this rates.', u'OK', number, i)
+                printlog(u'Finished collceting this rates.', u'OK', number, i, logfile)
                 return
-            printlog(u'Try to analyz Json.', u'waiting', number, i)
+            printlog(u'Try to analyz Json.', u'waiting', number, i, logfile)
             mytable = pd.read_json(newjson) #不使用to_sql的原因是评论过于复杂，to_sql构造的sql语句常常出错
-            printlog(u'Succeed in analyzing Json.', u'OK', number, i)
+            printlog(u'Succeed in analyzing Json.', u'OK', number, i, logfile)
             for item in mytable.values:
                 sql = u"INSERT INTO "+unicode(db_config["tablename"])+u" VALUES ("
                 for j in item:
@@ -98,16 +99,16 @@ def rateList2mysql(itemId,sellerId):
                     db.ping(True)
                 except:
                     #再次连接数据库
-                    printlog(u'Try to connect Mysql again.', db_config, number, i)
+                    printlog(u'Try to connect Mysql again.', db_config, number, i, logfile)
                     db = MySQLdb.connect(db_config["hostname"],
                                          db_config["username"],
                                          db_config["password"],
                                          db_config["databasename"],
                                          charset='utf8')
-                    printlog(u'Succeed in connecting Mysql.', u'OK', number, i)
+                    printlog(u'Succeed in connecting Mysql.', u'OK', number, i, logfile)
                 # 使用cursor()方法获取操作游标 
                 cursor = db.cursor()
-                printlog(u'Try to executing the SQL:', u'waiting',  number, i)
+                printlog(u'Try to executing the SQL:', u'waiting',  number, i, logfile)
                 try:
                     #使用execute方法执行SQL语句
                     cursor.execute(sql)
@@ -115,44 +116,41 @@ def rateList2mysql(itemId,sellerId):
                     # 提交到数据库执行
                     db.commit()
                 except:
-                #Rollback in case there is any error
-                    printlog(u'Failed to execute the SQL. The SQL is:', sql, number, i)
-                    db.rollback()
-                printlog(u'Succeed in executing the SQL:', u'OK', number, i)
+                    printlog(u'Failed to execute the SQL. The SQL is:', sql, number, i, logfile)
+                    exit(0)	#调试时打开此句。
+                printlog(u'Succeed in executing the SQL:', u'OK', number, i, logfile)
             oldjson = newjson
 #END OF rateList2mysql
 
-#打开日志文件
-logfile = open('rateListLog.txt', 'a')
-printlog(u'Opened logfile.', u'OK', 0, 0)
+
 # 打开数据库连接
-printlog(u'Try to connect Mysql.',db_config, 0, 0)
 db = MySQLdb.connect(db_config["hostname"],
                      db_config["username"],
                      db_config["password"],
                      db_config["databasename"],
                      charset='utf8')
-printlog(u'Succeed in connecting Mysql.', u'OK', 0, 0)
 # 抓取数据
-number = 0 #已经爬取过数据的商品的个数
-rateList2mysql(44090725053, 725677994)
-'''
 itemIdList = open('itemId.txt', 'r').readlines()
 sellerIdList = open('sellerId.txt', 'r').readlines()
-number = 0 #已经爬取过数据的商品的个数
+number = 0 #已经爬取过数据的（包括正在爬的）商品的数目
 for (itemId,selllerId) in zip(itemIdList, sellerIdList)[number:]:
+    time.sleep(2)	#谨慎起见
     number += 1
-    print u'第['+str(number)+u']个商品：'
-    time.sleep(2)
-    rateList2mysql(int(itemId), int(selllerId))
-    #exit(0)
-'''
+    #打开日志文件
+    logfile = open('rateListLog{0}.txt'.format(number), 'a')
+    printlog(u'Start collceting this rates.', u'OK', number, 0, logfile)
+    rateList2mysql(int(itemId), int(selllerId), logfile)
+    #关闭日志文件
+    printlog(u'Close log file.', u'OK', number, 0, logfile)
+    logfile.close()
+    #删除前一个的前一个商品的日志文件，避免最后日志文件过大
+    try:
+        os.remove('rateListLog{0}.txt'.format(number-2))
+    except OSError:
+        pass	#万一删除失败了也不再纠结于此
+
 #关闭数据库连接
 db.close()
-printlog(u'Close database connection.', u'OK', 0, 0)
-#关闭日志文件
-printlog(u'Close log file.', u'OK', 0, 0)
-logfile.close()
 
 '''
 创建数据库时就设置好字符编码，防止中文乱码
