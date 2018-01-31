d42682dd69bae151e024108224e3f586621d0506
werner <me@werner.wiki>
2018-01-31 10:49:46 +0800

    update bike_sharing.py

--- PythonMachineLearningCookbook/Chapter01/bike_sharing.py
+++ PythonMachineLearningCookbook/Chapter01/bike_sharing.py
@@ -27,11 +27,11 @@ def plot_feature_importances(feature_impoerances, title, feature_names):
 	plt.show()
 
 # 数据加载函数
-def load_dataset(filename):
+def load_dataset(filename, upper):
 	file_reader = csv.reader(open(filename, 'r'), delimiter=',')
 	X, y = [], []
 	for row in file_reader:
-		X.append(row[2:13])
+		X.append(row[2:upper])
 		y.append(row[-1])
 	# 提取特征名称
 	feature_names = np.array(X[0])
@@ -39,20 +39,22 @@ def load_dataset(filename):
 	return np.array(X[1:]).astype(np.float32), np.array(y[1:]).astype(np.float32), feature_names
 
 # 读取数据并打乱顺序并划分数据
-X, y, feature_names = load_dataset("bike_day.csv")
+X, y, feature_names = load_dataset("bike_day.csv", 13)
+#X, y, feature_names = load_dataset("bike_hour.csv", 14)
+
 X, y = shuffle(X, y, random_state = 7)
 X_train, X_test, y_train, y_test = cross_validation.train_test_split(X, y, test_size=0.1, random_state=0)
 
 rf_regressor = RandomForestRegressor(n_estimators=1000, #评估器（决策树）数量
 	                                                     max_depth=10, #每个决策树的最大深度
-	                                                     min_samples_split=1) #决策树分裂一个节点需要用到的最小数量样本
+	                                                     min_samples_split=2) #决策树分裂一个节点需要用到的最小数量样本
 rf_regressor.fit(X_train, y_train)
 
 # 评价随机森林回归器的训练效果
 y_pred = rf_regressor.predict(X_test)
 mse = mean_squared_error(y_test, y_pred)
 evs = explained_variance_score(y_test, y_pred)
-print(u"均方误差\t{0:.8f}".format(mse_dt))
-print(u"解释方差分\t{0:.8f}".format(evs_dt))
+print(u"均方误差\t{0:.8f}".format(mse))
+print(u"解释方差分\t{0:.8f}".format(evs))
 
-plot_feature_importances(rf_regressor.feature_impoerances_, 'Random Forest regressor', feature_names)
+plot_feature_importances(rf_regressor.feature_importances_, 'Random Forest regressor', feature_names)
