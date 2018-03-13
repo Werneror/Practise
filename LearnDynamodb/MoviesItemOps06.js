f096f80be4f22a328ca98eb64ce667b3713d802b
werner <me@werner.wiki>
2018-03-13 11:05:31 +0800

    添加了学习Dynamodb时写的代码

---
+++ LearnDynamodb/MoviesItemOps06.js
@@ -0,0 +1,33 @@
+var AWS = require("aws-sdk");
+AWS.config.update({
+    region: "us-west-2",
+    endpoint: "http://localhost:8000"
+});
+
+var docClient = new AWS.DynamoDB.DocumentClient();
+
+var table = "Movies";
+
+var year = 2015;
+var title = "The Big New Movie";
+
+var params = {
+    TableName: table,
+    Key:{
+        "year": year,
+        "title": title
+    },
+    ConditionExpression: "info.rating <= :val",
+    ExpressionAttributeValues:{
+        ":val": 50.0
+    }
+};
+
+console.log("Attemptin a conditional delete...");
+docClient.delete(params, function(err, data){
+    if(err){
+        console.error("Unable to update item. Error JSON:", JSON.stringify(err, null, 2));
+    }else{
+        console.log("DeleteItem succeeded:", JSON.stringify(data, null, 2));
+    }
+});
---
+++ LearnDynamodb/README.md
@@ -0,0 +1,16 @@
+- LearnDynamodb
+
+这些是我[学习用Node.js操作DynamoDB](https://docs.aws.amazon.com/zh_cn/amazondynamodb/latest/developerguide/GettingStarted.NodeJs.html)时写的代码。
+
+- [MoviesCreateTable.js](MoviesCreateTable.js) 创建表
+- [MoviesLoadData.js](MoviesLoadData.js) 载入数据
+- [MoviesItemOps01.js](MoviesItemOps01.js) 创建新项目
+- [MoviesItemOps02.js](MoviesItemOps02.js) 读取项目
+- [MoviesItemOps03.js](MoviesItemOps03.js) 更新项目
+- [MoviesItemOps04.js](MoviesItemOps04.js) 递增一个原子计数器
+- [MoviesItemOps05.js](MoviesItemOps05.js) 更新项目 (有条件)
+- [MoviesItemOps06.js](MoviesItemOps06.js) 删除项目
+- [MoviesQuery01.js](MoviesQuery01.js) 查询-根据分区键
+- [MoviesQuery02.js](MoviesQuery02.js) 查询-根据分区键和排序键
+- [MoviesScan.js](MoviesScan.js) 扫描
+- [MoviesDeleteTable.js](MoviesDeleteTable.js) 删除表
